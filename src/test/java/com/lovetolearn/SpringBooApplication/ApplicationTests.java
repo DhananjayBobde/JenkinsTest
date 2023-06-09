f841dad9cc69ddda5f7c98cc036d39763082e864
DhananjayBobde <113025710+DhananjayBobde@users.noreply.github.com>
2023-06-09 19:22:21 +0530

    check in

--- src/test/java/com/lovetolearn/SpringBooApplication/ApplicationTests.java
+++ src/test/java/com/lovetolearn/SpringBooApplication/ApplicationTests.java
@@ -1,20 +1,20 @@
-//package com.lovetolearn.SpringBooApplication;
-//
-//import org.junit.jupiter.api.Test;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//@SpringBootTest
-//class ApplicationTests {
-//	public static final Logger logger = LoggerFactory.getLogger(ApplicationTests.class);
-//
-//	@Test
-//	void contextLoads() {
-//		logger.info("in test class");
-//		assertEquals(true,true);
-//	}
-//
-//}
+package com.lovetolearn.SpringBooApplication;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class ApplicationTests {
+	public static final Logger logger = LoggerFactory.getLogger(ApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("in test class");
+		assertEquals(true,true);
+	}
+
+}
